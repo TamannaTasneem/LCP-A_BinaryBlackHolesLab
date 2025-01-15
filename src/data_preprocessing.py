ac5cc0ed3a1bb64b07883e734e87c34a92b1462b
Alberto Casellato <alberto.casellato@studenti.unipd.it>
2025-01-15 11:35:04 +0100

    Started to work on the first two tasks.

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -2,5 +2,16 @@ import pandas as pd
 import os
 
 def load_data(folder):
-    for filename in os.listdir(folder):
-        print(filename)
+    dict_dataset = {}
+    for filename1 in os.listdir(folder):
+        folder_path = os.path.join(folder, filename1)
+        list_dataset = {}
+
+        for filename2 in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename2)
+            df = pd.read_csv(file_path, delimiter=" ", skiprows=2)
+            list_dataset[filename2.split("_")[2].split(".txt")[0]] = df
+        
+        dict_dataset[filename1] = list_dataset
+    
+    return dict_dataset
